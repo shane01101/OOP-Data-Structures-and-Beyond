107ce5d4c2ff7a889489ad401ec7ffdb0e5645fd
Shane Honanie <shaneh425@gmail.com>
2018-06-11 18:50:09 -0700

    Week 2 Complete

--- Data Structures and Performance/MOOCTextEditor/src/document/BasicDocument.java
+++ Data Structures and Performance/MOOCTextEditor/src/document/BasicDocument.java
@@ -36,7 +36,8 @@ public class BasicDocument extends Document
 	{
 		//TODO: Implement this method in week 2 according to the comments above.  
 		// See the Module 2 support videos if you need help.
-	    return 0;
+		List<String> words = getTokens("[a-zA-Z]+");
+	    return words.size();
 	}
 	
 	/**
@@ -56,7 +57,9 @@ public class BasicDocument extends Document
 	{
 	    //TODO: Implement this method.  See the Module 2 support videos 
         // if you need help.
-        return 0;
+		List<String> sentences = getTokens("[^.?!]+");
+		
+	    return sentences.size();
 	}
 	
 	/**
@@ -81,7 +84,14 @@ public class BasicDocument extends Document
 		// expression for the syllable counting.  We recommend you implement 
 		// the helper function countSyllables in Document.java using a loop, 
 		// and then call it here on each word.
-        return 0;
+		
+		List<String> tokens = getTokens("[a-zA-Z]+");
+		int totalSyllables = 0;
+		for (String word : tokens)
+		{
+			totalSyllables += countSyllables(word);
+		}
+		return totalSyllables;
 	}
 	
 	
