d0177e14e05ee69ac86b8d6b76c5b7d873b10ae1
Shane Honanie <shaneh425@gmail.com>
2018-06-24 18:08:22 -0700

    Week 6 Edit Distance Insertions Deletions Completed

--- Data Structures and Performance/MOOCTextEditor/src/spelling/NearbyWords.java
+++ Data Structures and Performance/MOOCTextEditor/src/spelling/NearbyWords.java
@@ -59,9 +59,8 @@ public class NearbyWords implements SpellingSuggest {
 
 				// if the item isn't in the list, isn't the original string, and
 				// (if wordsOnly is true) is a real word, add to the list
-				if(!currentList.contains(sb.toString()) && 
-						(!wordsOnly||dict.isWord(sb.toString())) &&
-						!s.equals(sb.toString())) {
+				if(!currentList.contains(sb.toString()) && (!wordsOnly||dict.isWord(sb.toString())) && !s.equals(sb.toString())) 
+				{
 					currentList.add(sb.toString());
 				}
 			}
@@ -75,8 +74,27 @@ public class NearbyWords implements SpellingSuggest {
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return
 	 */
-	public void insertions(String s, List<String> currentList, boolean wordsOnly ) {
+	public void insertions(String s, List<String> currentList, boolean wordsOnly ) 
+	{
 		// TODO: Implement this method  
+		// for each letter in s + 1 and for all possible insertion characters
+		for(int index = 0; index <= s.length(); index++)
+		{
+			for(int charCode = (int)'a'; charCode <= (int)'z'; charCode++) 
+			{
+				// use StringBuffer for an easy interface to permuting the 
+				// letters in the String
+				StringBuffer sb = new StringBuffer(s);
+				sb.insert(index, (char)charCode);
+	
+				// if the item isn't in the list, isn't the original string, and
+				// (if wordsOnly is true) is a real word, add to the list
+				if(!currentList.contains(sb.toString()) && (!wordsOnly||dict.isWord(sb.toString())) && !s.equals(sb.toString())) 
+				{
+					currentList.add(sb.toString());
+				}
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
@@ -86,8 +104,25 @@ public class NearbyWords implements SpellingSuggest {
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return
 	 */
-	public void deletions(String s, List<String> currentList, boolean wordsOnly ) {
+	public void deletions(String s, List<String> currentList, boolean wordsOnly ) 
+	{
 		// TODO: Implement this method
+		// for each letter in s and for all possible deletion characters
+		for(int index = 0; index < s.length(); index++)
+		{
+	
+			// use StringBuffer for an easy interface to permuting the 
+			// letters in the String
+			StringBuffer sb = new StringBuffer(s);
+			sb.deleteCharAt(index);
+	
+			// if the item isn't in the list, isn't the original string, and
+			// (if wordsOnly is true) is a real word, add to the list
+			if(!currentList.contains(sb.toString()) && (!wordsOnly||dict.isWord(sb.toString())) && !s.equals(sb.toString())) 
+			{
+				currentList.add(sb.toString());
+			}
+		}
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
